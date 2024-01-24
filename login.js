723fd3e241b70ecb3286265c946816e6d2fb8236
maniARJ1 <126232800+maniARJ1@users.noreply.github.com>
2024-01-24 16:10:00 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+
+        function login() {
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
+
+            // Hier kannst du die Logik für die Überprüfung von Benutzername und Passwort implementieren.
+            // Zum Beispiel: if (username === 'beispiel' && password === 'passwort') { ... }
+        }
